8bfb1314f3f23ffb69f64bc94ade526dbbd4cf4f
jbyman <jakebyman@yahoo.com>
2015-11-16 02:39:55 -0500

    Heroku init

---
+++ CourseBuddy/CourseDetailsVC.h
@@ -0,0 +1,13 @@
+//
+//  CourseDetailsVC.h
+//  CourseBuddy
+//
+//  Created by Jake Byman on 11/15/15.
+//  Copyright © 2015 Jake Byman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CourseDetailsVC : UIViewController
+
+@end
